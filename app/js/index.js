f095a2e347cf98faae07105c61a824ef615e94af
Hunter <wil_dahl@yahoo.com>
2016-09-13 21:28:17 -0700

    working on remove functionality

--- app/js/index.js
+++ app/js/index.js
@@ -4,11 +4,20 @@ document.addEventListener(
     false
 );
 
+var task = {
+    name: '',
+    step: {
+        index: 1,
+        description: ''
+    }
+}
+
 function init(){
     document.querySelector('#newButton').addEventListener('click', showScreen.bind(null,'newHowTo'));
     document.querySelector('#cancel').addEventListener('click', showScreen.bind(null, 'main'));
     document.querySelector('#finished').addEventListener('click', finished);
     document.querySelector('#newStep').addEventListener('click', newStep);
+    document.querySelector('#clear').addEventListener('click', clearAll);
 
     showScreen('main');
 }
@@ -24,9 +33,33 @@ function showScreen(id){
 }
 
 function finished(){
+    task.name = document.getElementById('taskTitle').value;
+    console.log(task.name);
+}
 
+function clearAll(){
+    var newHowTo = document.getElementById('newHowTo');
+    x = document.getElementsByClassName('newStep');
+    console.log(x.length);
+    console.log(x);
+    for(var i = 0; i < x.length; i++)
+    {
+        x.value = ''
+        newHowTo.removeChild(x[i]);
+    }
+    document.getElementById('taskTitle').value = '';
 }
 
 function newStep(){
-    
+    //console.log('newStep');
+    var input = document.createElement('input');
+    input.id = 'stepInfo' + task.step.index;
+    input.className = 'newStep';
+    //console.log(input);
+    var newHowTo = document.getElementById('newHowTo');
+    newHowTo.appendChild(input);
+    task.step.description = document.getElementById('stepInfo'+task.step.index).value;
+    task.step.index++;
+    console.log(task.step.description);
+    //console.log(task);
 }
